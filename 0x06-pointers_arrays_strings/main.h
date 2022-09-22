72c4da86d6d48c2a6f0ef63c0f40c5af17d2172a
Kidahfr <kayodeayodele93@yahoo.com>
2022-09-22 13:29:14 -0700

    My task commit on main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 int main(void);
-void print_line(char *c, int s, int l)
 
 #endif
